dc556014ec1f50d89e2ea946bf98b60ba90810bc
Suraj <bgs071998@gmail.com>
2020-07-08 21:50:06 +0530

    initial commit

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+
+	$toEmail = "surajbg07@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
+?>
